f7825e11421fb0df8162a653786582add4ca7a2b
Arpan Sadaphal <111455969+arpansadaphal@users.noreply.github.com>
2026-03-03 12:16:38 +0000

    updated features

--- portfolio-lab/src/app/components/Features.tsx
+++ portfolio-lab/src/app/components/Features.tsx
@@ -32,7 +32,7 @@ export default function Features() {
 
         {/* Section Heading */}
         <motion.div
-          initial={{ opacity: 0, y: 40 }}
+        //   initial={{ opacity: 0, y: 40 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
           viewport={{ once: true }}
